15f5beaaa9eb674cdf2ce273430454d4fa27a19a
Vill Ain <vill99green@gmail.com>
2025-01-20 18:12:27 -0800

    Some updates, add grading, still lots of work to do

--- analyze.py
+++ analyze.py
@@ -4,6 +4,18 @@ import sys
 
 file_name = "log_snippet.txt"
 #file_name = "eqlog_Vill_P1999Green.txt"
+# TODO - need a way to tell it which chain to analyze when 
+#   there are many. Probably default to most recent
+#   Add feature to specify a date to start after
+#   Also maybe a date range, so a start and end date
+# TODO - Summary doesn't understand skips
+#   doesn't understand subsequent cleric firing correct for late heal
+#   doesn't understand tank switching
+#   When wrong target is healed, subsequent cleric gets switched target also
+#   Doesn't know if you ranged a heal or ducked or went oom
+#   If you double tap because you went early, it penalizes
+#file_name = "tuna_snippet_2025_01_12.txt"
+#file_name = "vindi_heals_2025_01_15.txt"
 
 string_format = "%a %b %d %H:%M:%S %Y"
 
@@ -45,7 +57,48 @@ class comparison():
     def __str__(self):
         output = " ".join([" In order:", str(self.ordered), "\n", "Delay:", str(self.delay), "\n","Target:", str(self.target)])
         return output
-    
+
+class cleric():
+    """
+    """
+    def __init__(self, name):
+        self.name = name
+        self.heals = 0
+        self.switched_targets = 0
+        self.out_of_order = 0
+        self.delay_diffs = []
+        self.grade = 100
+    def __str__(self):
+        self.do_grading()
+        output = "Name: " + self.name + "\n"
+        output += "Total Heals: " + str(self.heals) + "\n"
+        output += "Switched Targets: " + str(self.switched_targets) + "\n"
+        output += "Out of Order: " + str(self.out_of_order) + "\n"
+        output += "Delay Diffs: " + str(self.delay_diffs) + "\n" 
+        output += "Grade: " + format(self.grade, ".2f") + "\n"
+
+        return output
+    def do_heal(self, switched=False, ordered=True, delay_diff=0):
+        self.heals += 1
+        if switched:
+            self.switched_targets += 1
+        if not ordered:
+            self.out_of_order += 1
+        if delay_diff != 0:
+            self.delay_diffs.append(delay_diff)
+    def do_grading(self):
+        demerits = 0
+        for diff in self.delay_diffs:
+            if diff > 1 or diff < -1:
+                demerits += 1
+            if diff == 1 or diff == -1:
+                demerits += 0.2
+        demerits += self.switched_targets
+        demerits += self.out_of_order
+        self.grade = (self.heals - demerits) / self.heals * 100
+
+
+
 def extract_date(line):
     """Get an everquest log date and turn it into a python date"""
     stripped = line.split("[")
@@ -128,38 +181,70 @@ def validate(diff, current, previous):
     if diff.target != "Same":
         print("** Target switch, was " + previous.target + " new target: " + current.target)
 
-
-# Start with checking set to False
-checking = False
-# A list of complete heals to populate
-chain = []
-# default the expected delay to 1
-expected_delay = 1
-
 # Parse the logfile
-with open(file_name) as logfile:
-    for line in logfile:
-        if start_checking(line):
-            checking = True
-        if stop_checking(line):
-            checking = False
-        if checking:
-            expected_delay = chain_speed(line, expected_delay)
-        if is_chain_line(line) and checking:
-            date = extract_date(line)
-            cleric = extract_cleric(line)
-            number = extract_number(line)
-            target = extract_target(line)
-            chain.append(ch(date, cleric, number, target, expected_delay))
+def parse():
+    # Start with checking set to False
+    checking = False
+    # A list of complete heals to populate
+    chain = []
+    # default the expected delay to 1
+    expected_delay = 1
+    with open(file_name) as logfile:
+        for line in logfile:
+            if start_checking(line):
+                checking = True
+            if stop_checking(line):
+                checking = False
+            if checking:
+                expected_delay = chain_speed(line, expected_delay)
+            if is_chain_line(line) and checking:
+                date = extract_date(line)
+                cleric = extract_cleric(line)
+                number = extract_number(line)
+                target = extract_target(line)
+                chain.append(ch(date, cleric, number, target, expected_delay))
+    return chain
+
+def insert_cleric(diff, heal, clerics):
+    clerics.append(cleric(heal.cleric))
+
+def add_heal(diff, heal, clerics):
+    updated = False
+    for index, cleric in enumerate(clerics):
+        if heal.cleric == cleric.name:
+            switched = False
+            if diff.target != "Same":
+                switched = True
+            delay = diff.delay - int(heal.expected_delay)
+            clerics[index].do_heal(switched, diff.ordered, delay)
+            updated = True
+    if not updated:
+        insert_cleric(diff, heal, clerics)
+        switched = False
+        if diff.target != "Same":
+            switched = True
+        delay = diff.delay - int(heal.expected_delay)
+        clerics[-1].do_heal(switched, diff.ordered, delay)
 
 
-previous = None  # Initialize the previous value
 
 # Output the analyzation
-for heal in chain:
-    if previous is not None:
-        print("CH:", heal)
-        diff = compare_sequential(heal, previous)
-        validate(diff, heal, previous)
-    previous = heal
+def analyze(chain, clerics):
+    previous = None  # Initialize the previous value
+    for heal in chain:
+        if previous is not None:
+            print("CH:", heal)
+            diff = compare_sequential(heal, previous)
+            validate(diff, heal, previous)
+            add_heal(diff, heal, clerics)
+        previous = heal
+    for cleric in clerics:
+        print(cleric)
 
+def main():
+    clerics = []
+    chain = parse()
+    analyze(chain, clerics)
+
+if __name__ == "__main__":
+    main()
